77840d70c1f24501d9d52e95e288b92ce8682d45
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2026-01-21 18:37:26 -0600

    Update yolk-run.sh

--- yolk-run.sh
+++ yolk-run.sh
@@ -2,8 +2,8 @@ NAME=gitpm-example-theme
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ $DIR == "~/.local/share/apps/$NAME"] then
-  XDG_CONFIG_HOME=~/.local/share/apps/$NAME/yolk yolk sync
+  yolk sync --yolk-dir "~/.local/share/apps/$NAME/yolk"
 fi
 if [ $DIR == "/opt/apps/$NAME"] then
-  XDG_CONFIG_HOME=/opt/apps/$NAME/yolk yolk sync
+  yolk sync --yolk-dir "/opt/apps/$NAME/yolk"
 fi
